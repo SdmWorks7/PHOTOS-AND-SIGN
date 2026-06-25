fc6ab24e2ab7e14b44c5369a0beb1b742c3c52b2
Saumyadeep Mishra <sdm.cord7@gmail.com>
2026-06-25 18:18:44 +0530

    added database with sqlalchemy

--- html learn/module 1/python_learning/main.py
+++ html learn/module 1/python_learning/main.py
@@ -1,20 +1,26 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from database import SessionLocal, User
 
-app=FastAPI()
+app = FastAPI()
 
-class Users(BaseModel):
-    name:str
-    age:int
-    city:str
-
-users = []
+class UserInput(BaseModel):
+    name: str
+    age: int
+    city: str
 
 @app.get("/users")
 def get_users():
+    db = SessionLocal()
+    users = db.query(User).all()
+    db.close()
     return {"users": users}
 
 @app.post("/users")
-def create_users(user:Users):
-    users.append(user)
-    return {"message":"user created succesfully","user":user}
+def create_user(user: UserInput):
+    db = SessionLocal()
+    new_user = User(name=user.name, age=user.age, city=user.city)
+    db.add(new_user)
+    db.commit()
+    db.close()
+    return {"message": "User created!"}
